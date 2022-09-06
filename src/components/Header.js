faf80f4e69a98beaeb4af9b634f29306c100c4ac
hiba-u <coder.hiba@gmail.com>
2022-09-06 12:11:41 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react"
+import lauraImg from "../images/Rectangle 90.png"
+import emailIcon from "../images/email-icon.png"
+import linkedinIcon from "../images/linkedin-vector.png"
+
+export default function Header(){
+    return (
+        <div>
+            <img id="laura-img" src={lauraImg} alt="Laura Smith"/>
+            <h1 id="title">Laura Smith</h1>
+            <h4 id="role">Frontend Developer</h4>
+            <p id="website">laurasmith.website</p>
+            <div id="btn-div">
+                <div id="email-btn">
+                    <img src={emailIcon}/>
+                    <p>Email</p>
+                </div>
+                <div id="linkedin-btn">
+                    <img src={linkedinIcon}/>
+                    <p>Linkedin</p>
+                </div>
+            </div>
+        </div>
+    )
+}

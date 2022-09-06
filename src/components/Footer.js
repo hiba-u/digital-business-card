3c6f186e40b30d419711839ef441f1f7652bd97a
hiba-u <coder.hiba@gmail.com>
2022-09-06 13:29:52 +0530

    Add styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from "react"
+import twitterIcon from "../images/Twitter Icon.png"
+import fbIcon from "../images/Facebook Icon.png"
+import instaIcon from "../images/Instagram Icon.png"
+import githubIcon from "../images/GitHub Icon.png"
 
 export default function Footer(){
     return (
-        <div>
-            <img src="" alt=""/>
-            <img src="" alt=""/>
-            <img src="" alt=""/>
-            <img src="" alt=""/>
+        <div id="footer">
+            <div id="icons">
+                <img src={twitterIcon} alt="Twitter"/>
+                <img src={fbIcon} alt="Facebook"/>
+                <img src={instaIcon} alt="Instagram"/>
+                <img src={githubIcon} alt="GitHub"/>
+            </div>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,21 @@ import linkedinIcon from "../images/linkedin-vector.png"
 
 export default function Header(){
     return (
-        <div>
+        <div id="header">
             <img id="laura-img" src={lauraImg} alt="Laura Smith"/>
             <h1 id="title">Laura Smith</h1>
             <h4 id="role">Frontend Developer</h4>
             <p id="website">laurasmith.website</p>
             <div id="btn-div">
-                <div id="email-btn">
-                    <img src={emailIcon}/>
-                    <p>Email</p>
-                </div>
-                <div id="linkedin-btn">
-                    <img src={linkedinIcon}/>
-                    <p>Linkedin</p>
+                <div id="buttons">
+                    <div id="email-btn">
+                        <img src={emailIcon}/>
+                        <p>Email</p>
+                    </div>
+                     <div id="linkedin-btn">
+                        <img src={linkedinIcon}/>
+                        <p>Linkedin</p>
+                    </div>
                 </div>
             </div>
         </div>

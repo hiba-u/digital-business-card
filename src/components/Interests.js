b60f4aa4ee85d0697877a1a2d7aeb395bfb45e43
hiba-u <coder.hiba@gmail.com>
2022-09-06 12:24:04 +0530

    Style Interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Interests(){
     return (
-        <div>
+        <div className="content-div">
             <h2>Interests</h2>
             <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </div>
